443e1502513fb12faadcc81af39bfb091e5381b5
Shaelyn Christie <shaelync@uvic.ca>
2026-07-16 10:49:48 -0700

    update

---
+++ butterMain.m
@@ -0,0 +1,76 @@
+%butterworth main
+
+%to do
+    %output the data into a plot
+    %tweak to handle GCMS data as well
+    %parallelize to do for loop as parfor
+
+function [dataOut] = butterMain(dataIn, opts)
+arguments 
+    dataIn
+    opts.stop = 0
+    opts.low = 0
+    opts.notches = []
+    opts.bw = []
+    opts.ordr = []
+    opts.cutoff = []
+end
+
+%call this to make the tensor
+[tensOut] = makeTensor(dataIn);
+szTens = size(tensOut);
+numMods = szTens(2);
+%specDataOut = [];
+Wns = [];
+Wnl = [];
+fs = dataIn.dataRate;
+
+if opts.stop
+    upLim = zeros(1, length(opts.notches)); %row vectors
+    dwnLim = zeros(1, length(opts.notches));
+
+    for i = 1:length(opts.notches)
+        upLim(i) = opts.notches(i) + opts.bw;
+        dwnLim(i) = opts.notches(i) - opts.bw;
+    end
+    edges = sort([upLim dwnLim]); %horizontal concat and 
+    % sorts into ascending order
+    Wns = edges/(fs/2);
+end
+if opts.low
+    Wnl = opts.cutoff/(fs/2);
+end
+dim1 = szTens(1);
+specDataOut = zeros(numMods*dim1, szTens(3));
+for ii = 1:numMods
+    %get the current modulation
+    curMod = squeeze(tensOut(:,ii,:)); 
+    [specDataFFT] = butterFilt(curMod,opts.stop,opts.low,opts.ordr,Wns,Wnl);
+    %specDataOut = [specDataOut; specDataFFT]; 
+    ind = (ii-1)*dim1 + (1:dim1);
+    specDataOut(ind,:) = specDataFFT;
+end
+
+%make the dataOut structure
+dataOut = dataIn;
+
+%just doing this to get the tensor out
+%dataIn.specdata = specDataOut; 
+
+%new tensor for 2d
+%if isfield(dataIn,'modTime')
+ %   [tensorBW] = makeTensor(dataIn);
+%end
+ticBW = sum(specDataOut, 2);
+
+%make the dataout structure complete
+%dataOut.tensor = tensorBW;
+dataOut.specdata = specDataOut;
+%infoFFT is just metadata about the FFT
+  %  info.notches = opts.notches;
+   % info.bandWidth = opts.bw;
+    %info.order = opts.ordr;
+%dataOut.info = info;
+dataOut.tic = ticBW;
+
+end
