98cf5cb98d7b88a7d964f3371bea451c3c4ef9ad
Shaelyn Christie <shaelync@uvic.ca>
2026-07-20 14:10:47 -0700

    update

---
+++ figure9.m
@@ -0,0 +1,21 @@
+
+gctens = makeTensor(g2d_ord2);
+gcchr = sum(gctens,3);
+gcticD1 = sum(gcchr,1);
+
+
+gntens = makeTensor(g2d);
+gnchr = sum(gntens,3);
+gnticD1 = sum(gnchr,1);
+
+figure; 
+yyaxis left
+plot(gnticD1); 
+hold on; 
+yyaxis right 
+plot(gcticD1);
+
+
+legend('Before','After')
+
+ 
