c54d99e47f11e7d9c6911124530f3690b187e6ae
Shaelyn Christie <shaelync@uvic.ca>
2026-07-31 13:39:20 -0700

    update

--- figure9.m
+++ figure9.m
@@ -1,21 +1,34 @@
 
-gctens = makeTensor(g_scale_IN);
+gctens = makeTensor(g_out60);
 gcchr = sum(gctens,3);
-gcticD1 = sum(gcchr,1);
+
+gcticD1_us = sum(gcchr,1);
+
+
 
 
 gntens = makeTensor(g2d);
 gnchr = sum(gntens,3);
 gnticD1 = sum(gnchr,1);
 
+gcticD1 = gcticD1_us * max(gnticD1)/max(gcticD1_us);
+
 figure; 
 yyaxis left
 plot(gnticD1); 
+ylabel('Intensity')
 hold on; 
 yyaxis right 
 plot(gcticD1);
+ylabel('Intensity')
+xlabel('Acquisitions')
+%ylabel('Intensity')
 
+lgd = legend('Before','After');
+lgd.FontSize = 14;
 
-legend('Before','After')
+ax = gca;
+ax.FontSize = 14;
+
 
  
